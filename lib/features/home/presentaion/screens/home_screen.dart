fd9805edd7d34655807912f35371dc99e5d8a1f9
Ahmed <ahmedmohameddx80@gmail.com>
2025-06-13 18:51:46 +0300

    build tap bar in home screen

--- lib/features/home/presentaion/screens/home_screen.dart
+++ lib/features/home/presentaion/screens/home_screen.dart
@@ -1,17 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:high_fidelity/core/utils/app_colors.dart';
 import 'package:high_fidelity/features/home/presentaion/widgets/banner_widget.dart';
 import 'package:high_fidelity/features/home/presentaion/widgets/head_of_home_screen_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final tabController = useTabController(initialLength: 5);
+    final selectedIndex = useState(0);
+
+    useEffect(() {
+      void listener() {
+        selectedIndex.value = tabController.index;
+      }
+
+      tabController.addListener(listener);
+      return () => tabController.removeListener(listener);
+    }, [tabController]);
+
+    final tabs = ["All Coffee", "Machiato", "Latte", "Cappuccino", "Espresso"];
+
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Column(children: [HeadOfHomeScreenWidget()]),
-          BannerWidget(),
+          Stack(
+            children: [
+              Column(
+                children: [HeadOfHomeScreenWidget(), SizedBox(height: 100.h)],
+              ),
+              const BannerWidget(),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(tabs.length, (index) {
+                  final isSelected = selectedIndex.value == index;
+                  return Padding(
+                    padding: EdgeInsets.only(right: 8.w),
+                    child: GestureDetector(
+                      onTap: () {
+                        tabController.animateTo(index);
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 200),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 12.w,
+                          vertical: 6.h,
+                        ),
+                        decoration: BoxDecoration(
+                          color:
+                              isSelected
+                                  ? AppColors.primaryColor
+                                  : Colors.transparent,
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                        child: Text(
+                          tabs[index],
+                          style: TextStyle(
+                            color:
+                                isSelected ? Colors.white : AppColors.darkColor,
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+          SizedBox(height: 16.h),
+          Expanded(
+            child: TabBarView(
+              controller: tabController,
+              children: const [
+                Center(
+                  child: Text(
+                    "All Coffee",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    "Machiato",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Center(
+                  child: Text("Latte", style: TextStyle(color: Colors.white)),
+                ),
+                Center(
+                  child: Text(
+                    "Cappuccino",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    "Espresso",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
